5a2c3db1f1fbed1d6404b33dbec391eb9dd9a02d
romanenko-v <romanenkov.iosdev@gmail.com>
2025-11-12 13:32:20 +0300

    fix print in linux

--- WheelOrder/App.swift
+++ WheelOrder/App.swift
@@ -1,11 +1,25 @@
 import Foundation
 
+#if os(Linux)
+import Glibc
+@inline(__always)
+private func disableStdIOBuffering() {
+    setvbuf(stdout, nil, _IONBF, 0)
+    setvbuf(stderr, nil, _IONBF, 0)
+}
+#else
+@inline(__always)
+private func disableStdIOBuffering() {}
+#endif
+
 @main
 struct App {
     static func main() async {
+        disableStdIOBuffering()
+
         let api = OzonSellerAPI(clientId: Config.OZON_CLIENT_ID, apiKey: Config.OZON_API_KEY)
         let cache = PostingCache()
-//        cache.clear()
+        // cache.clear()
 
         let worker = OrderChatWorker(api: api,
                                      cache: cache,
